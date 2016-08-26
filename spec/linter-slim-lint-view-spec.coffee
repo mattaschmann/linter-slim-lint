574a5ed54bfa91b0930adf259b2d5c929b4a9d80
Matt Aschmann <matt.aschmann@scoutapp.com>
2016-08-26 16:30:12 -0600

    Convert to javascript, don't have time to learn coffeescript right now

--- spec/linter-slim-lint-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-LinterSlimLintView = require '../lib/linter-slim-lint-view'
-
-describe "LinterSlimLintView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/linter-slim-lint-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterSlimLintView from '../lib/linter-slim-lint-view';
+
+describe('LinterSlimLintView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
