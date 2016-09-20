9b946cf0b2f033495c987aea5501502992e04e3c
Matt Aschmann <matt.aschmann@scoutapp.com>
2016-09-20 13:25:18 -0600

    Got linting working

--- spec/linter-slim-lint-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LinterSlimLintView from '../lib/linter-slim-lint-view';
-
-describe('LinterSlimLintView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
